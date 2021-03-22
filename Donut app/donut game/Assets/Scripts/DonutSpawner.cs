63a6d27ea9699b8da2d68e25682cd5a591908a0c
James Lyddon <59798913+JamesLyddon@users.noreply.github.com>
2021-03-22 17:07:52 +0000

    explosion and force added

--- Donut app/donut game/Assets/Scripts/DonutSpawner.cs
+++ Donut app/donut game/Assets/Scripts/DonutSpawner.cs
@@ -22,7 +22,22 @@ public class DonutSpawner : MonoBehaviour
     {
         var tempDonut = Instantiate(donut, new Vector3((Random.Range(-0.5f, 0.5f)), Random.Range(1.0f, 2.5f), Random.Range(-0.15f, 0.15f)), Quaternion.identity);
         yield return new WaitForSeconds(3);
-        Instantiate(explosionEffect, tempDonut.transform.position, tempDonut.transform.rotation);
+        var tempExplosion = Instantiate(explosionEffect, tempDonut.transform.position, tempDonut.transform.rotation);
+        tempExplosion.GetComponent<AudioSource>().pitch = Random.Range(0.5f, 5f);
+        tempExplosion.GetComponent<AudioSource>().Play();
+        Collider[] colliders = Physics.OverlapSphere(tempDonut.transform.position, 1);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(200, tempDonut.transform.position, 1);
+            }
+        }
+
         Destroy(tempDonut);
+        yield return new WaitForSeconds(1);
+        Destroy(tempExplosion);
     }
 }
